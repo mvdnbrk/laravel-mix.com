393b3e98961e44b56e3cbbd7a81eefdd845d835d
Mark van den Broek <mvdnbrk@gmail.com>
2018-07-30 20:35:10 +0200

    Adds meta description.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="description" content="An elegant wrapper around Webpack for the 80% use case.">
 
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
