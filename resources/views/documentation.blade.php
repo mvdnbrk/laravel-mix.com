e82bb8fe0bb6857d919479a9bd2b52b9187266c9
Mark van den Broek <mvdnbrk@gmail.com>
2018-09-03 20:16:26 +0200

    Place carbon ads in it's own partial.

--- resources/views/documentation.blade.php
+++ resources/views/documentation.blade.php
@@ -8,14 +8,7 @@
     <div class="container mx-auto">
         <div class="flex">
             <section class="flex flex-col lg:w-1/4 xl:w-1/5 p-8 border-r">
-                <div>
-                    <script
-                        async
-                        type="text/javascript"
-                        src="//cdn.carbonads.com/carbon.js?serve=CK7DV27M&placement=laravel-mixcom"
-                        id="_carbonads_js">
-                    </script>
-                </div>
+                @include('partials.carbonads')
                 <div class="docs-index">
                     {!! $index !!}
                 </div>
---
+++ resources/views/partials/carbonads.blade.php
@@ -0,0 +1,8 @@
+<div>
+    <script
+        async
+        type="text/javascript"
+        src="//cdn.carbonads.com/carbon.js?serve=CK7DV27M&placement=laravel-mixcom"
+        id="_carbonads_js">
+    </script>
+</div>
