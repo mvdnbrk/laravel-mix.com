cd8a33c7f193342f5af0e37efcc068554415231d
Mark van den Broek <mvdnbrk@gmail.com>
2018-09-29 18:20:54 +0200

    Adds leading-normal.

--- resources/views/documentation.blade.php
+++ resources/views/documentation.blade.php
@@ -13,7 +13,7 @@
                     {!! $index !!}
                 </div>
             </nav>
-            <article id="content" class="w-full lg:w-3/4 xl:w-3/5 pt-10 px-6 min-h-(screen-16)">
+            <article id="content" class="w-full lg:w-3/4 xl:w-3/5 pt-10 px-6 min-h-(screen-16) leading-normal">
                 {!! $content !!}
             </article>
             <div class="hidden pt-4 px-6 xl:flex flex-col xl:w-1/5 text-sm items-center lg:max-h-(screen-22) pin-22 lg:sticky">
