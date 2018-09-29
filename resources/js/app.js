8ff98e4785ec72e416e3ad3f6bd11ed56c2b1d0f
Mark van den Broek <mvdnbrk@gmail.com>
2018-09-29 17:23:54 +0200

    Clean up app.js.

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,27 +3,8 @@ import Prism from 'prismjs'
 
 Prism.highlightAll()
 
+// Highlights active link.
 let current = document.querySelectorAll(".docs-index a[href='" + window.location.href + "']");
 if (current.length) {
     current[0].classList.toggle('is-active');
 }
-
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
-// require('./bootstrap');
-
-// window.Vue = require('vue');
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-// const app = new Vue({
-//     el: '#app'
-// });
