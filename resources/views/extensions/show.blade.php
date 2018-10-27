2fc09abdc9ef123b1ca4ff265a79b3c9f0948ed1
Mark van den Broek <mvdnbrk@gmail.com>
2018-10-27 20:54:28 +0200

    Adds README to an extension page.

--- resources/views/extensions/show.blade.php
+++ resources/views/extensions/show.blade.php
@@ -55,23 +55,19 @@
                 </div>
             @endif
         </div>
-        <div class="flex justify-center py-6">
-            @include('partials.carbonads')
-        </div>
-        <div class="flex justify-center py-6">
-            <blockquote class="note">
-                <div>
-                    <svg class="w-6 h-6 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                        <path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 5h2v6H9V5zm0 8h2v2H9v-2z"/>
-                    </svg>
-                </div>
-                <p>Work in progress here...<br>Please come back in a while.</p>
-            </blockquote>
-        </div>
+
+        <article class="py-12 lg:px-10 markdown-body">
+            {!! $extension->readme !!}
+        </article>
+
         <div class="py-6 text-center border-t">
             <a href="{{ route('extensions.index') }}">back to index</a>
         </div>
      </div>
+
+    @push('scripts')
+        <script type="text/javascript" src="{{ mix('js/app.js') }}"></script>
+    @endpush
 
     @include('partials.footer')
 @endsection
