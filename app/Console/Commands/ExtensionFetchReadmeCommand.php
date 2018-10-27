026695a9ae7d292aff697b3581a606039477fb45
Mark van den Broek <mvdnbrk@gmail.com>
2018-10-27 16:58:56 +0200

    Adds command to fetch readme files from the repository.

---
+++ app/Console/Commands/ExtensionFetchReadmeCommand.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Extension;
+use App\Jobs\FetchReadme;
+use Illuminate\Console\Command;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class ExtensionFetchReadmeCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'extension:readme {name? : The name of the package }';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Fetch the readme file for a package from the repository.';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        Extension::when($this->argument('name'), function($query) {
+            return $query->where('name', $this->argument('name'));
+        })
+        ->orderBy('name')
+        ->chunk(10, function($extensions) {
+            $extensions->each(function ($extension) {
+                $this->comment("Processing: {$extension->name}");
+                dispatch(new FetchReadme($extension));
+            });
+        });
+
+        $this->info('Jobs to fetch the readme files from the repository created successfully.');
+    }
+}
