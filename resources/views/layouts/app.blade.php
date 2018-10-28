5d4bdba0907b218e45f28d1726d305ed9589c241
Mark van den Broek <mvdnbrk@gmail.com>
2018-10-28 20:54:37 +0100

    Formatting.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,9 +2,10 @@
 <html lang="{{ app()->getLocale() }}">
     <head>
         <meta charset="utf-8">
+        <title>{{ $title ?? config('app.name', 'Laravel Mix') }}</title>
+        <meta name="description" content="{{ $description ?? 'An elegant wrapper around Webpack for the 80% use case.' }}">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <meta name="description" content="{{ $description ?? 'An elegant wrapper around Webpack for the 80% use case.' }}">
 
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
         <link rel="icon" sizes="16x16 32x32" href="/images/favicons/favicon.ico">
@@ -13,19 +14,14 @@
         <link rel="icon" type="image/png" sizes="96x96" href="/images/favicons/96x96.png">
         <link rel="apple-touch-icon-precomposed" href="/images/favicons/152x152.png">
 
-        <title>{{ $title ?? config('app.name', 'Laravel Mix') }}</title>
-
         @include('partials.schema-organization')
 
         {!! Analytics::render() !!}
     </head>
-
     <body>
-
-    <div id="app">
-        @yield('body')
-    </div>
-
-    @stack('scripts')
+        <div id="app">
+            @yield('body')
+        </div>
+        @stack('scripts')
     </body>
 </html>
