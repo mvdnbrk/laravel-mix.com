3e5da918b7b6bdd4189f7dba6537fa8d27d01f5e
Mark van den Broek <mvdnbrk@gmail.com>
2018-11-29 22:21:28 +0100

    Adds documentation for v3.0.

--- build/docs.sh
+++ build/docs.sh
@@ -5,6 +5,7 @@ docs=${base}/storage/docs
 cd ${docs}/1.7 && git pull origin 1.7
 cd ${docs}/2.0 && git pull origin 2.0
 cd ${docs}/2.1 && git pull origin 2.1
+cd ${docs}/3.0 && git pull origin 3.0
 cd ${docs}/master && git pull origin master
 
 cd ${base}/current && php artisan page-cache:clear
