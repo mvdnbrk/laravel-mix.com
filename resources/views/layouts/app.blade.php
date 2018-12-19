7f6fa57895a53034624c01fe5fe9431cad5796a2
Mark van den Broek <mvdnbrk@gmail.com>
2018-12-19 22:02:28 +0100

    Adds favicons.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,23 +3,24 @@
     <head>
         <meta charset="utf-8">
         <title>{{ $title ?? config('app.name', 'Laravel Mix') }}</title>
-        <meta name="description" content="{{ $description ?? 'An elegant wrapper around Webpack for the 80% use case.' }}">
         @if (isset($canonical))
         <link rel="canonical" href="{{ url($canonical) }}" />
         @endif
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/site.webmanifest">
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+        <meta name="apple-mobile-web-app-title" content="Laravel MIx">
+        <meta name="application-name" content="Laravel MIx">
+        <meta name="msapplication-TileColor" content="#ffffff">
+        <meta name="theme-color" content="#ffffff">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-        <link rel="icon" sizes="16x16 32x32" href="/images/favicons/favicon.ico">
-        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicons/16x16.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicons/32x32.png">
-        <link rel="icon" type="image/png" sizes="96x96" href="/images/favicons/96x96.png">
-        <link rel="apple-touch-icon-precomposed" href="/images/favicons/152x152.png">
-
+        <meta name="description" content="{{ $description ?? 'An elegant wrapper around Webpack for the 80% use case.' }}">
         @include('partials.schema-organization')
         {{ Breadcrumbs::view('breadcrumbs::json-ld') }}
-
         {!! Analytics::render() !!}
     </head>
     <body>
