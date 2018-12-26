0ebe87d009881244be85f87cc6e05aebfd0f8470
Mark van den Broek <mvdnbrk@gmail.com>
2018-12-26 13:04:33 +0100

    Code style.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,42 +1,46 @@
 const mix = require('laravel-mix')
+
 require('laravel-mix-purgecss')
 
 mix.babelConfig({
     plugins: [
-        ['prismjs', {
-            'languages': ['bash', 'css', 'diff', 'javascript', 'markup', 'php', 'sass', 'yaml']
-        }]
+        [
+            'prismjs',
+            {
+                'languages': ['bash', 'css', 'diff', 'javascript', 'markup', 'php', 'sass', 'yaml']
+            }
+        ]
     ]
 });
 
 mix.js('resources/js/app.js', 'public/js')
-  .postCss('resources/css/app.css', 'public/css')
-  .options({
-    postCss: [
-      require('postcss-import')(),
-      require('tailwindcss')('./tailwind.js'),
-      require('postcss-nesting')(),
-    ]
-  })
-  .purgeCss({
-    whitelist: [
-        'h1',
-        'h2',
-        'h3',
-        'h4',
-        'li',
-        'ul',
-        'code',
-        'pre',
-        'table',
-        'blockquote',
-        'lg:max-h-(screen-22)',
-        'min-h-(screen-16)',
-    ],
-    whitelistPatterns: [/carbon.*/],
-    whitelistPatternsChildren: [/^docs-index$/, /^markdown-body$/, /^token$/]
-  })
+    .postCss('resources/css/app.css', 'public/css')
+    .options({
+        postCss: [
+            require('postcss-import')(),
+            require('tailwindcss')('./tailwind.js'),
+            require('postcss-nesting')(),
+        ]
+    })
+    .purgeCss({
+        whitelist: [
+            'h1',
+            'h2',
+            'h3',
+            'h4',
+            'li',
+            'ul',
+            'code',
+            'pre',
+            'table',
+            'blockquote',
+            'lg:max-h-(screen-22)',
+            'min-h-(screen-16)',
+        ],
+        whitelistPatterns: [/carbon.*/],
+        whitelistPatternsChildren: [/^docs-index$/, /^markdown-body$/, /^token$/]
+    })
 
 if (mix.inProduction()) {
-  mix.version()
+    mix.version()
 }
