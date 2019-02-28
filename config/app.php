4083534f65d467edf05744495a44a064815bc087
Mark van den Broek <mvdnbrk@gmail.com>
2019-02-28 12:21:37 +0100

    Removes Telescope.

--- config/app.php
+++ config/app.php
@@ -160,7 +160,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\HorizonServiceProvider::class,
-        App\Providers\TelescopeServiceProvider::class,
         App\Providers\NovaServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
