7b70e231d71a41bc44a848242913d77281384199
Mark van den Broek <mvdnbrk@gmail.com>
2019-03-01 10:18:45 +0100

    Removes telescope command.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,6 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('telescope:prune')->daily();
         $schedule->command('extension:stats')->daily();
     }
 
