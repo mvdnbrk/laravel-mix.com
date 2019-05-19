ef22b09e91a8c34e51736f23b53eb5781a5af60f
Mark van den Broek <mvdnbrk@gmail.com>
2019-05-19 17:22:19 +0200

    Updates Tailwindcss to ^1.0.

--- resources/views/documentation.blade.php
+++ resources/views/documentation.blade.php
@@ -6,7 +6,7 @@
 @section('body')
     @include('partials.header')
 
-    <div class="max-w-3xl mx-auto px-6">
+    <div class="w-full max-w-screen-xl mx-auto px-6">
         <div class="lg:flex -mx-6">
             <nav id="nav" class="hidden w-full lg:block lg:w-1/4 xl:w-1/5 px-6 pt-4 lg:border-r z-40 lg:max-h-(screen-22) pin-22 lg:sticky">
 
@@ -16,11 +16,11 @@
                         @continue
                     @endif
                     @if($currentVersion !== $version)
-                    <a class="px-2 py-1 mx-1 bg-grey-lighter text-sm text-grey rounded border hover:border-grey hover:text-grey-darker" href="{{ $url }}">
+                    <a class="px-2 py-1 mx-1 bg-gray-200 text-sm text-gray-500 rounded border hover:border-gray-500 hover:text-gray-700" href="{{ $url }}">
                         {{ $version }}
                     </a>
                     @else
-                    <div class="px-2 py-1 mx-1 bg-grey-light text-sm text-grey-darker rounded border border-grey">
+                    <div class="px-2 py-1 mx-1 bg-gray-400 text-sm text-gray-700 rounded border border-gray-500">
                         {{ $version }}
                     </div>
                     @endif
@@ -37,7 +37,7 @@
             <div class="hidden pt-4 px-6 xl:flex flex-col xl:w-1/5 text-sm items-center lg:max-h-(screen-22) pin-22 lg:sticky">
                 @include('partials.carbonads')
                 <a
-                    class="flex items-center justify-center mt-4 px-5 py-2 rounded border text-grey-darker no-underline hover:bg-grey-lighter hover:border-grey"
+                    class="flex items-center justify-center mt-4 px-5 py-2 rounded border text-gray-700 no-underline hover:bg-gray-200 hover:border-gray-500"
                     href="{{ config('documentation.repository.url') }}/edit/{{ $currentVersion }}/{{ $page }}.md"
                     target="_blank"
                 >
