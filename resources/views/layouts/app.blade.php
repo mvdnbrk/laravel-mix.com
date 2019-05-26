7c84374e8bc99d6a2e1f253472f34791e506687b
Mark van den Broek <mvdnbrk@gmail.com>
2019-05-26 19:33:21 +0200

    Adds viewport.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,6 +2,7 @@
 <html lang="{{ app()->getLocale() }}">
     <head>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <title>{{ $title ?? config('app.name', 'Laravel Mix') }}</title>
         @if (isset($canonical))
         <link rel="canonical" href="{{ url($canonical) }}" />
