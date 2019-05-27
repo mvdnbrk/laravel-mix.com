89e59c9d58d608bb0f6a1d09a6fecb595f6449aa
Mark van den Broek <mvdnbrk@gmail.com>
2019-05-27 13:01:36 +0200

    Adds Carbon Ads component.

---
+++ resources/js/components/CarbonAds.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    render(createElement) {
+        return createElement('script', {
+            'attrs': {
+                'async': true,
+                'id': '_carbonads_js',
+                'src': '//cdn.carbonads.com/carbon.js?serve=CK7DV27M&placement=laravel-mixcom',
+            }
+        });
+    }
+}
+</script>
