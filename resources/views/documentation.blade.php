914f74fc772d54f9678bd0049e22ba505303c801
Mark van den Broek <mvdnbrk@gmail.com>
2019-05-28 22:04:06 +0200

    Adds dropdown component to views.

--- resources/views/documentation.blade.php
+++ resources/views/documentation.blade.php
@@ -4,31 +4,15 @@
 ])
 
 @section('body')
-@include('partials.header')
+@include('partials.header', [
+    'pageExistsInVersions' => $pageExistsInVersions,
+    'currentVersion' => $currentVersion,
+])
 
 <div class="w-full max-w-screen-xl mx-auto px-6">
     <div class="lg:flex -mx-6">
-        <nav id="nav" class="hidden w-full lg:block lg:w-1/4 xl:w-1/5 px-6 pt-4 lg:border-r z-40 lg:max-h-(screen-22) pin-22 lg:sticky">
-            <div class="flex -mx-1 mt-2 mb-4">
-            @foreach($pageExistsInVersions as $version => $url)
-                @if($version == 'master')
-                    @continue
-                @endif
-                @if($currentVersion !== $version)
-                <a class="px-2 py-1 mx-1 bg-gray-200 text-sm text-gray-500 rounded border hover:border-gray-500 hover:text-gray-700" href="{{ $url }}">
-                    {{ $version }}
-                </a>
-                @else
-                <div class="px-2 py-1 mx-1 bg-gray-400 text-sm text-gray-700 rounded border border-gray-500">
-                    {{ $version }}
-                </div>
-                @endif
-            @endforeach
-            </div>
-
-            <div class="docs-index">
-                {!! $index !!}
-            </div>
+        <nav id="nav" class="docs-index hidden w-full lg:block lg:w-1/4 xl:w-1/5 pt-6 px-6 lg:border-r z-10 lg:max-h-(screen-22) pin-22 lg:sticky">
+            {!! $index !!}
         </nav>
         <article id="content" class="w-full lg:w-3/4 xl:w-3/5 pt-10 px-6 lg:px-12 min-h-(screen-16) markdown-body" v-pre>
             {!! $content !!}
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,12 +1,25 @@
 <scroll-indicator height="4px" color="#F27CC2"></scroll-indicator>
-<header class="flex items-center bg-white h-16 z-50 top-0 sticky border-t-4 border-b">
+<header class="flex items-center bg-white h-16 z-20 top-0 sticky border-t-4 border-b">
     <div class="container mx-auto">
         <div class="flex items-center justify-between px-6 lg:px-16">
             <a href="{{ config('app.url') }}">
                 @include('svg.laravel-mix-logo', ['class' => 'h-8 xl:h-10'])
             </a>
             @if(! isset($hide_menu))
-                <toggle-menu></toggle-menu>
+            <toggle-menu></toggle-menu>
+            <div class="hidden lg:block">
+                <dropdown v-cloak>
+                    <template v-slot:trigger>
+                        <button type="button" class="flex text-gray-500 hover:text-gray-700 focus:text-gray-500 focus:outline-none">
+                            {{ $currentVersion == 'master' ? '' : 'v' }}{{ $currentVersion }}
+                            @include('svg.icons.chevron-down', ['class' => 'fill-current w-6 h-6 ml-1'])
+                        </button>
+                    </template>
+                    @foreach($pageExistsInVersions as $version => $url)
+                    <li><a href="{{ $url }}" class="px-8 text-center leading-loose hover:bg-gray-200 block">{{ $version }}</a></li>
+                    @endforeach
+                </dropdown>
+            </div>
             @endif
         </div>
     </div>
