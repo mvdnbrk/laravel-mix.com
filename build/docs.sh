c45a60ed9b3c570204d60e1c5bf2623ec9813113
Mark van den Broek <mvdnbrk@gmail.com>
2019-07-13 12:22:13 +0200

    Adds 4.1 to build script.

--- build/docs.sh
+++ build/docs.sh
@@ -7,6 +7,7 @@ cd ${docs}/2.0 && git pull origin 2.0
 cd ${docs}/2.1 && git pull origin 2.1
 cd ${docs}/3.0 && git pull origin 3.0
 cd ${docs}/4.0 && git pull origin 4.0
+cd ${docs}/4.1 && git pull origin 4.1
 cd ${docs}/master && git pull origin master
 
 cd ${base}/current && php artisan page-cache:clear
