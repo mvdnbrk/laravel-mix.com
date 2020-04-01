24e2e571b77f5840f361ed450c12ed7fc7b6d2b0
Mark van den Broek <mvdnbrk@gmail.com>
2020-04-01 21:14:01 +0200

    Remove docblocks

--- app/Http/Controllers/DocumentationBaseController.php
+++ app/Http/Controllers/DocumentationBaseController.php
@@ -6,19 +6,8 @@ use App\Documentation;
 
 class DocumentationBaseController
 {
-    /**
-     * The documentation repository.
-     *
-     * @var \App\Documentation
-     */
     protected Documentation $documentation;
 
-    /**
-     * Create a new controller instance.
-     *
-     * @param  \App\Documentation  $documentation
-     * @return void
-     */
     public function __construct(Documentation $documentation)
     {
         $this->documentation = $documentation;
