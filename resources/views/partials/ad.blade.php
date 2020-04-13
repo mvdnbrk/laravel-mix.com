62c89ab018ef9d9c897e3812bbc052fcf8f2844d
Mark van den Broek <mvdnbrk@gmail.com>
2020-04-13 22:43:03 +0200

    Move ad to partial

---
+++ resources/views/partials/ad.blade.php
@@ -0,0 +1,19 @@
+<div class="lg:px-10">
+    <div class="flex py-2 justify-center items-center bg-gray-100 rounded-b-lg border-t text-sm">
+        <a href="https://m.do.co/c/7a24c68b1e6d" class="text-gray-600 hover:text-digitalocean-blue">
+            @include('svg.icons.digital-ocean', ['class' => 'mr-4 fill-current w-6'])
+        </a>
+        <a href="https://m.do.co/c/7a24c68b1e6d" class="text-gray-600 hover:text-digitalocean-blue">
+            Try <span class="font-medium">DigitalOcean</span>
+        </a>
+        <a href="https://m.do.co/c/7a24c68b1e6d" taget="_blank" class=" ml-4 text-gray-600 hover:text-digitalocean-blue" rel="noopener noreferrer">
+            Simple cloud hosting, built for developers.
+        </a>
+        <a href="https://m.do.co/c/7a24c68b1e6d" taget="_blank" class="ml-4 text-gray-600 hover:text-digitalocean-blue" rel="noopener noreferrer">
+            Get started now and get <span class="font-medium">$100 credit</span>.
+        </a>
+        <a href="https://m.do.co/c/7a24c68b1e6d">
+            <span class="bg-white border rounded-lg py-1 px-3 font-medium text-gray-600 text-2xs ml-4 hover:border-digitalocean-blue hover:text-digitalocean-blue">AD</span>
+        </a>
+    </div>
+</div>
