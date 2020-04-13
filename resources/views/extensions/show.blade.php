f71f82b67a2a182baffff180da86d1d102aaa3ca
Mark van den Broek <mvdnbrk@gmail.com>
2020-04-13 22:08:54 +0200

    Add DigitalOcean ad

--- resources/views/extensions/show.blade.php
+++ resources/views/extensions/show.blade.php
@@ -9,7 +9,7 @@
 ])
 
     <div class="max-w-screen-xl mx-auto px-6">
-        <div class="py-12 border-b">
+        <div class="py-12">
             <h1 class="m-0 pb-2 text-center">{{ $extension->title }}</h1>
             <div class="text-center">{{ $extension->description }}</div>
             <div class="flex justify-center mt-6">
@@ -56,6 +56,26 @@
                 @endforeach
             </div>
             @endif
+        </div>
+
+        <div class="lg:px-10">
+            <div class="flex py-2 justify-center items-center bg-gray-100 rounded-b-lg border-t text-sm">
+                <a href="https://m.do.co/c/7a24c68b1e6d" class="text-gray-600 ">
+                    @include('svg.icons.digital-ocean', ['class' => 'mr-4 fill-current w-6'])
+                </a>
+                <a href="https://m.do.co/c/7a24c68b1e6d" class="text-gray-600 ">
+                    Try <span class="font-medium">DigitalOcean</span>
+                </a>
+                <a href="https://m.do.co/c/7a24c68b1e6d" taget="_blank" class=" ml-4 text-gray-600 hover:text-digitalocean-blue" rel="noopener noreferrer">
+                    Simple cloud hosting, built for developers.
+                </a>
+                <a href="https://m.do.co/c/7a24c68b1e6d" taget="_blank" class="ml-4 text-gray-600 hover:text-digitalocean-blue" rel="noopener noreferrer">
+                    Get started now and get <span class="font-medium">$100 credit</span>.
+                </a>
+                <a href="https://m.do.co/c/7a24c68b1e6d">
+                    <span class="bg-white border rounded-lg py-1 px-3 font-medium text-gray-600 text-2xs ml-4 hover:border-digitalocean-blue hover:text-digitalocean-blue">AD</span>
+                </a>
+            </div>
         </div>
 
         @if($extension->hasLocalReadme())
