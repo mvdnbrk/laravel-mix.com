2291b1aa1f2ecb5b86f1191658db10194540cb96
Mark van den Broek <mvdnbrk@gmail.com>
2020-04-13 22:08:06 +0200

    Add rel tag with noopener and noreferrer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,10 +1,10 @@
 <footer class="flex items-center justify-center mt-16 mb-24 text-gray-500 text-xs">
     proudly hosted with
-    <a href="https://m.do.co/c/7a24c68b1e6d" class="text-gray-500 hover:text-blue-500">
+    <a href="https://m.do.co/c/7a24c68b1e6d" class="text-gray-500 hover:text-blue-500" rel="noopener noreferrer">
         @include('svg.digital-ocean-logo', ['class' => 'mx-2 fill-current w-24'])
     </a>
     managed with
-    <a href="https://ploi.io" class="text-sm text-gray-500 font-helvetica ml-2 hover:text-ploi-blue">
+    <a href="https://ploi.io" class="text-sm text-gray-500 font-helvetica ml-2 hover:text-ploi-blue" rel="noopener noreferrer">
         <strong>Ploi</strong>.io
     </a>
 </footer>
