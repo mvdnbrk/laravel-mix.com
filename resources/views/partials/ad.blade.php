7c9b2d56f0810c51f3c60c81ef121297ccc98dcd
Mark van den Broek <mvdnbrk@gmail.com>
2020-04-13 23:53:26 +0200

    Add target blank

--- resources/views/partials/ad.blade.php
+++ resources/views/partials/ad.blade.php
@@ -1,9 +1,9 @@
 <div class="lg:px-10">
     <div class="flex py-2 justify-center items-center bg-gray-100 rounded-b-lg border-t text-sm">
-        <a href="https://m.do.co/c/7a24c68b1e6d" class="text-gray-600 hover:text-digitalocean-blue">
+        <a href="https://m.do.co/c/7a24c68b1e6d" taget="_blank" class="text-gray-600 hover:text-digitalocean-blue">
             @include('svg.icons.digital-ocean', ['class' => 'mr-4 fill-current w-6'])
         </a>
-        <a href="https://m.do.co/c/7a24c68b1e6d" class="text-gray-600 hover:text-digitalocean-blue">
+        <a href="https://m.do.co/c/7a24c68b1e6d" taget="_blank" class="text-gray-600 hover:text-digitalocean-blue">
             Try <span class="font-medium">DigitalOcean</span>
         </a>
         <a href="https://m.do.co/c/7a24c68b1e6d" taget="_blank" class=" ml-4 text-gray-600 hover:text-digitalocean-blue" rel="noopener noreferrer">
@@ -12,7 +12,7 @@
         <a href="https://m.do.co/c/7a24c68b1e6d" taget="_blank" class="ml-4 text-gray-600 hover:text-digitalocean-blue" rel="noopener noreferrer">
             Get started now and get <span class="font-medium">$100 credit</span>.
         </a>
-        <a href="https://m.do.co/c/7a24c68b1e6d">
+        <a href="https://m.do.co/c/7a24c68b1e6d" taget="_blank">
             <span class="bg-white border rounded-lg py-1 px-3 font-medium text-gray-600 text-2xs ml-4 hover:border-digitalocean-blue hover:text-digitalocean-blue">AD</span>
         </a>
     </div>
