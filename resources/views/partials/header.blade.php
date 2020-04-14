c8c6d51f0f858c8e09ccdc2d1291990d84600dea
Mark van den Broek <mvdnbrk@gmail.com>
2020-04-14 08:00:16 +0200

    Adds links in header to docs/extensions

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -5,22 +5,33 @@
             <a href="{{ config('app.url') }}">
                 @include('svg.laravel-mix-logo', ['class' => 'h-8 xl:h-10'])
             </a>
-            @if(! isset($hide_menu))
-            <toggle-menu></toggle-menu>
-            <div class="hidden lg:block">
-                <dropdown v-cloak>
-                    <template v-slot:trigger>
-                        <button type="button" class="flex text-gray-500 hover:text-gray-700 focus:text-gray-500 focus:outline-none">
-                            {{ $currentVersion == 'master' ? '' : 'v' }}{{ $currentVersion }}
-                            @include('svg.icons.chevron-down', ['class' => 'fill-current w-6 h-6 ml-1'])
-                        </button>
-                    </template>
-                    @foreach($versionsContainingPage as $version)
-                    <li><a href="{{ route('documentation.show', ['page' => $page, 'version' => $version]) }}" class="px-8 text-center leading-loose hover:bg-gray-200 block">{{ $version }}</a></li>
-                    @endforeach
-                </dropdown>
+            <div class="flex">
+                @if(! isset($hide_menu))
+                <toggle-menu></toggle-menu>
+                <div class="hidden lg:block">
+                    <dropdown v-cloak>
+                        <template v-slot:trigger>
+                            <button type="button" class="flex text-gray-500 hover:text-gray-700 focus:text-gray-500 focus:outline-none">
+                                {{ $currentVersion == 'master' ? '' : 'v' }}{{ $currentVersion }}
+                                @include('svg.icons.chevron-down', ['class' => 'fill-current w-6 h-6 ml-1'])
+                            </button>
+                        </template>
+                        @foreach($versionsContainingPage as $version)
+                        <li><a href="{{ route('documentation.show', ['page' => $page, 'version' => $version]) }}" class="px-8 text-center leading-loose hover:bg-gray-200 block">{{ $version }}</a></li>
+                        @endforeach
+                    </dropdown>
+                </div>
+                @endif
+
+                <div class="hidden lg:flex">
+                    <a href="{{ route('documentation.root') }}" class="mx-2 text-gray-500 hover:text-gray-700">
+                        documentation
+                    </a>
+                    <a href="{{ route('extensions.index') }}" class="mx-2 text-gray-500 hover:text-gray-700">
+                        extensions
+                    </a>
+                </div>
             </div>
-            @endif
         </div>
     </div>
 </header>
