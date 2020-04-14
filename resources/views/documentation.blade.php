ec9458b6eade5e9ecf3a89c2e65f14d980f1869d
Mark van den Broek <mvdnbrk@gmail.com>
2020-04-14 08:02:31 +0200

    Adjust font color for "edit this page" button

--- resources/views/documentation.blade.php
+++ resources/views/documentation.blade.php
@@ -20,7 +20,7 @@
         <div class="hidden pt-4 px-6 xl:flex flex-col xl:w-1/5 text-sm items-center lg:max-h-(screen-22) pin-22 lg:sticky">
             <carbon-ads></carbon-ads>
             <a
-                class="flex items-center justify-center mt-4 px-5 py-2 rounded border text-gray-700 hover:bg-gray-200 hover:border-gray-500"
+                class="flex items-center justify-center mt-4 px-5 py-2 rounded border text-gray-500 hover:text-gray-700 hover:bg-gray-200 hover:border-gray-500"
                 href="https://github.com/mvdnbrk/laravel-mix-docs/edit/{{ $currentVersion }}/{{ $page }}.md"
                 target="_blank"
             >
