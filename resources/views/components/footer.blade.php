bbc3dd6dfc3794165e33be498493d70a73cac27e
Mark van den Broek <mvdnbrk@gmail.com>
2020-04-23 14:15:41 +0200

    Move Fathom arrow to icons folder

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -9,6 +9,6 @@
     </a>
     analytics by
     <a href="https://usefathom.com/ref/FI15PB" class="flex ml-2 text-sm font-bold font-fathom text-gray-500 hover:text-fathom-indigo" rel="noopener noreferrer">
-        Fathom Analytics @include('svg.fathom-arrow', ['class' => 'ml-1 w-3'])
+        Fathom Analytics @include('svg.icons.fathom-arrow', ['class' => 'ml-1 w-3'])
     </a>
 </footer>
