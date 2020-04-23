e1486510d7b4342df76a60e5442cc30dccacd69c
Mark van den Broek <mvdnbrk@gmail.com>
2020-04-23 11:13:32 +0200

    Formatting

--- resources/views/partials/schema-organization.blade.php
+++ resources/views/partials/schema-organization.blade.php
@@ -1,12 +1,14 @@
-<script type="application/ld+json">@php
-    echo json_encode([
-        '@context' => 'http://schema.org/',
-        '@type' => 'Organization',
-        'name' => 'Laravel Mix',
-        'logo' => 'https://laravel-mix.com/images/logo.png',
-        'sameAs' => [
-            'https://github.com/JeffreyWay/laravel-mix',
-        ],
-        'url' => 'https://laravel-mix.com',
-    ], JSON_UNESCAPED_SLASHES);
-@endphp</script>
+<script type="application/ld+json">
+@php
+echo json_encode([
+    '@context' => 'http://schema.org/',
+    '@type' => 'Organization',
+    'name' => 'Laravel Mix',
+    'logo' => 'https://laravel-mix.com/images/logo.png',
+    'sameAs' => [
+        'https://github.com/JeffreyWay/laravel-mix',
+    ],
+    'url' => 'https://laravel-mix.com',
+], JSON_UNESCAPED_SLASHES);
+@endphp
+</script>
