89812003b218becb950eed07a9b8d307e26dec1f
Mark van den Broek <mvdnbrk@gmail.com>
2020-04-23 14:39:28 +0200

    Add Fathom ad

---
+++ resources/views/components/ads/fathom.blade.php
@@ -0,0 +1,19 @@
+<div class="lg:px-10">
+    <div class="flex py-2 justify-center items-center bg-gray-100 rounded-b-lg border-t text-sm">
+        <a href="https://usefathom.com/ref/FI15PB" taget="_blank">
+            @include('svg.icons.fathom-arrow', ['class' => 'mr-4 w-6'])
+        </a>
+        <a href="https://usefathom.com/ref/FI15PB" taget="_blank" class="text-gray-600 hover:text-fathom-indigo">
+            Try <span class="text-fathom font-bold">Fathom Analytics</span>
+        </a>
+        <a href="https://usefathom.com/ref/FI15PB" taget="_blank" class="ml-4 text-gray-600 hover:text-fathom-indigo" rel="noopener noreferrer">
+            Simple analytics for bloggers & businesses.
+        </a>
+        <a href="https://usefathom.com/ref/FI15PB" taget="_blank" class="ml-4 text-gray-600 hover:text-fathom-indigo" rel="noopener noreferrer">
+            Get started now and get <span class="font-medium">$10 credit</span>.
+        </a>
+        <a href="https://usefathom.com/ref/FI15PB" taget="_blank">
+            <span class="bg-white border rounded-lg py-1 px-3 font-medium text-gray-600 text-2xs ml-4 hover:border-fathom-indigo hover:text-fathom-indigo">SPONSOR</span>
+        </a>
+    </div>
+</div>
