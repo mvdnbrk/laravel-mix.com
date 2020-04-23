39886fdf0df9dcb129d0350506aa35b1d50e3ead
Mark van den Broek <mvdnbrk@gmail.com>
2020-04-23 12:31:18 +0200

    Add Fathom logo svg

---
+++ resources/views/svg/fathom-arrow.blade.php
@@ -0,0 +1,7 @@
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 1000" class="{{ $class ?? '' }}">
+    <g transform="translate(88 176)">
+      <path d="M0 245.334l368 122.668-73.6-220.8z" fill="#a89ef5"/>
+      <path d="M613.333 368L735.999 0l-368 122.67-73.6 24.537L367.999 368l220.8 73.607z" fill="#533feb"/>
+      <path d="M588.799 441.6l-220.8-73.6 122.665 368z" fill="#8efdd2"/>
+    </g>
+</svg>
