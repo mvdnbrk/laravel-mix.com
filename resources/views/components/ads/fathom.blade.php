f22df251084abaa5ad95702e5031f631f8aa726b
Mark van den Broek <mvdnbrk@gmail.com>
2020-04-27 21:31:03 +0200

    Add goal

--- resources/views/components/ads/fathom.blade.php
+++ resources/views/components/ads/fathom.blade.php
@@ -1,18 +1,18 @@
 <div class="lg:px-10">
     <div class="flex py-2 justify-center items-center bg-gray-100 rounded-b-lg border-t text-sm">
-        <a href="https://usefathom.com/ref/FI15PB" taget="_blank">
+        <a href="https://usefathom.com/ref/FI15PB" taget="_blank" onclick="fathom('trackGoal', '7D8GVLAA', 0);">
             @include('svg.icons.fathom-arrow', ['class' => 'mr-4 w-6'])
         </a>
-        <a href="https://usefathom.com/ref/FI15PB" taget="_blank" class="text-gray-600 hover:text-fathom-indigo">
+        <a href="https://usefathom.com/ref/FI15PB" taget="_blank" class="text-gray-600 hover:text-fathom-indigo" onclick="fathom('trackGoal', '7D8GVLAA', 0);">
             Try <span class="text-fathom font-bold">Fathom Analytics</span>
         </a>
-        <a href="https://usefathom.com/ref/FI15PB" taget="_blank" class="ml-4 text-gray-600 hover:text-fathom-indigo" rel="noopener noreferrer">
+        <a href="https://usefathom.com/ref/FI15PB" taget="_blank" class="ml-4 text-gray-600 hover:text-fathom-indigo" rel="noopener noreferrer" onclick="fathom('trackGoal', '7D8GVLAA', 0);">
             Simple analytics for bloggers & businesses.
         </a>
-        <a href="https://usefathom.com/ref/FI15PB" taget="_blank" class="ml-4 text-gray-600 hover:text-fathom-indigo" rel="noopener noreferrer">
+        <a href="https://usefathom.com/ref/FI15PB" taget="_blank" class="ml-4 text-gray-600 hover:text-fathom-indigo" rel="noopener noreferrer" onclick="fathom('trackGoal', '7D8GVLAA', 0);">
             Get started now and get <span class="font-medium">$10 credit</span>.
         </a>
-        <a href="https://usefathom.com/ref/FI15PB" taget="_blank">
+        <a href="https://usefathom.com/ref/FI15PB" taget="_blank" onclick="fathom('trackGoal', '7D8GVLAA', 0);">
             <span class="bg-white border rounded-lg py-1 px-3 font-medium text-gray-600 text-2xs ml-4 hover:border-fathom-indigo hover:text-fathom-indigo">SPONSOR</span>
         </a>
     </div>
