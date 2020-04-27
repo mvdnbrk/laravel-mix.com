aeacbf657f31e634b4d3105e871bfde599a495a9
Mark van den Broek <mvdnbrk@gmail.com>
2020-04-27 21:21:53 +0200

    Add target blank

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,14 +1,14 @@
 <footer class="flex items-center justify-center h-16 mt-16 mb-24 text-gray-500 text-xs">
     proudly hosted with
-    <a href="https://m.do.co/c/7a24c68b1e6d" class="text-gray-500 hover:border-digitalocean-blue" rel="noopener noreferrer">
+    <a href="https://m.do.co/c/7a24c68b1e6d" class="text-gray-500 hover:border-digitalocean-blue" rel="noopener noreferrer" target="_blank">
         @include('svg.digital-ocean-logo', ['class' => 'mx-2 fill-current w-24'])
     </a>
     managed with
-    <a href="https://ploi.io" class="mx-2 text-sm text-gray-500 font-helvetica hover:text-ploi-blue" onclick="fathom('trackGoal', 'KUEOOI7D', 0);" rel="noopener">
+    <a href="https://ploi.io" class="mx-2 text-sm text-gray-500 font-helvetica hover:text-ploi-blue" onclick="fathom('trackGoal', 'KUEOOI7D', 0);" rel="noopener" target="_blank">
         <strong>Ploi</strong>.io
     </a>
     analytics by
-    <a href="https://usefathom.com/ref/FI15PB" class="flex ml-2 text-sm font-bold font-fathom text-gray-500 hover:text-fathom-indigo" rel="noopener noreferrer">
+    <a href="https://usefathom.com/ref/FI15PB" class="flex ml-2 text-sm font-bold font-fathom text-gray-500 hover:text-fathom-indigo" rel="noopener noreferrer" target="_blank">
         Fathom Analytics @include('svg.icons.fathom-arrow', ['class' => 'ml-1 w-3'])
     </a>
 </footer>
