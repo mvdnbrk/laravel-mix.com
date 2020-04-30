92f314e052ebb205641e63ac6f26e094b35be358
Mark van den Broek <mvdnbrk@gmail.com>
2020-04-30 14:50:01 +0200

    Add Fathom goal

--- resources/views/components/ads/digitalocean.blade.php
+++ resources/views/components/ads/digitalocean.blade.php
@@ -1,18 +1,43 @@
 <div class="lg:px-10">
     <div class="flex py-2 justify-center items-center bg-gray-100 rounded-b-lg border-t text-sm">
-        <a href="https://m.do.co/c/7a24c68b1e6d" taget="_blank" class="text-gray-600 hover:text-digitalocean-blue">
+        <a
+            href="https://m.do.co/c/7a24c68b1e6d"
+            onclick="fathom('trackGoal', 'TVUSHJYA', 0);"
+            rel="noopener noreferrer"
+            target="_blank" class="text-gray-600 hover:text-digitalocean-blue"
+        >
             @include('svg.icons.digital-ocean', ['class' => 'mr-4 fill-current w-6'])
         </a>
-        <a href="https://m.do.co/c/7a24c68b1e6d" taget="_blank" class="text-gray-600 hover:text-digitalocean-blue">
+        <a
+            href="https://m.do.co/c/7a24c68b1e6d"
+            onclick="fathom('trackGoal', 'TVUSHJYA', 0);"
+            rel="noopener noreferrer"
+            target="_blank" class="text-gray-600 hover:text-digitalocean-blue"
+        >
             Try <span class="font-medium">DigitalOcean</span>
         </a>
-        <a href="https://m.do.co/c/7a24c68b1e6d" taget="_blank" class=" ml-4 text-gray-600 hover:text-digitalocean-blue" rel="noopener noreferrer">
+        <a
+            href="https://m.do.co/c/7a24c68b1e6d"
+            onclick="fathom('trackGoal', 'TVUSHJYA', 0);"
+            rel="noopener noreferrer"
+            target="_blank" class=" ml-4 text-gray-600 hover:text-digitalocean-blue"
+        >
             Simple cloud hosting, built for developers.
         </a>
-        <a href="https://m.do.co/c/7a24c68b1e6d" taget="_blank" class="ml-4 text-gray-600 hover:text-digitalocean-blue" rel="noopener noreferrer">
+        <a
+            href="https://m.do.co/c/7a24c68b1e6d"
+            onclick="fathom('trackGoal', 'TVUSHJYA', 0);"
+            rel="noopener noreferrer"
+            target="_blank" class="ml-4 text-gray-600 hover:text-digitalocean-blue"
+        >
             Get started now and get <span class="font-medium">$100 credit</span>.
         </a>
-        <a href="https://m.do.co/c/7a24c68b1e6d" taget="_blank">
+        <a
+            href="https://m.do.co/c/7a24c68b1e6d"
+            onclick="fathom('trackGoal', 'TVUSHJYA', 0);"
+            rel="noopener noreferrer"
+            target="_blank"
+        >
             <span class="bg-white border rounded-lg py-1 px-3 font-medium text-gray-600 text-2xs ml-4 hover:border-digitalocean-blue hover:text-digitalocean-blue">SPONSOR</span>
         </a>
     </div>
