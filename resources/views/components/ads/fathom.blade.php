fa9388696f1fb5afbb868f7a304522f1a16d4c08
Mark van den Broek <mvdnbrk@gmail.com>
2020-05-02 12:33:47 +0200

    Fix typo

--- resources/views/components/ads/fathom.blade.php
+++ resources/views/components/ads/fathom.blade.php
@@ -4,7 +4,7 @@
             href="https://usefathom.com/ref/FI15PB"
             onclick="fathom('trackGoal', '7D8GVLAA', 0);"
             rel="noopener noreferrer"
-            taget="_blank"
+            target="_blank"
         >
             @include('svg.icons.fathom-arrow', ['class' => 'mr-4 w-6'])
         </a>
@@ -12,7 +12,7 @@
             href="https://usefathom.com/ref/FI15PB"
             onclick="fathom('trackGoal', '7D8GVLAA', 0);"
             rel="noopener noreferrer"
-            taget="_blank"
+            target="_blank"
             class="text-gray-600 hover:text-fathom-indigo"
         >
             Try <span class="text-fathom font-bold">Fathom Analytics</span>
@@ -21,7 +21,7 @@
             href="https://usefathom.com/ref/FI15PB"
             onclick="fathom('trackGoal', '7D8GVLAA', 0);"
             rel="noopener noreferrer"
-            taget="_blank"
+            target="_blank"
             class="ml-4 text-gray-600 hover:text-fathom-indigo"
         >
             Simple analytics for bloggers & businesses.
@@ -30,7 +30,7 @@
             href="https://usefathom.com/ref/FI15PB"
             onclick="fathom('trackGoal', '7D8GVLAA', 0);"
             rel="noopener noreferrer"
-            taget="_blank"
+            target="_blank"
             class="ml-4 text-gray-600 hover:text-fathom-indigo"
         >
             Get started now and get <span class="font-medium">$10 credit</span>.
@@ -39,7 +39,7 @@
             href="https://usefathom.com/ref/FI15PB"
             onclick="fathom('trackGoal', '7D8GVLAA', 0);"
             rel="noopener noreferrer"
-            taget="_blank"
+            target="_blank"
         >
             <span class="bg-white border rounded-lg py-1 px-3 font-medium text-gray-600 text-2xs ml-4 hover:border-fathom-indigo hover:text-fathom-indigo">SPONSOR</span>
         </a>
