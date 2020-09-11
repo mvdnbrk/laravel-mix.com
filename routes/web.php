1fb6d57d755b5444b0aa717c47dda28522b705ae
Mark van den Broek <mvdnbrk@gmail.com>
2020-09-11 14:09:12 +0200

    Remove docblock

--- routes/web.php
+++ routes/web.php
@@ -6,17 +6,6 @@ use App\Http\Controllers\DocumentationRootController;
 use App\Http\Controllers\ExtensionsController;
 use App\Http\Controllers\HomepageController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::middleware(['page-cache'])->group(function () {
     Route::get('/', [HomepageController::class, 'show'])->name('homepage');
 
