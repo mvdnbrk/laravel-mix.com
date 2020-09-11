95130892b4705d12baa71509d8e5da5724430993
Mark van den Broek <mvdnbrk@gmail.com>
2020-09-11 09:57:29 +0200

    Sort tailwind classes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,16 @@
 @extends('layouts.app')
 
 @section('body')
-    <div class="min-h-screen flex items-center justify-center border-t-4">
+    <div class="flex items-center justify-center min-h-screen border-t-4">
         <div class="flex flex-col items-center">
             @include('svg.laravel-mix-logo', ['class' => 'w-64'])
 
-            <p class="mt-6 text-lg px-4 text-center leading-normal">An elegant wrapper around Webpack for the 80% use case.</p>
+            <p class="px-4 mt-6 text-lg leading-normal text-center">An elegant wrapper around Webpack for the 80% use case.</p>
 
-            <div class="md:flex mt-16">
+            <div class="mt-16 md:flex">
                 <a
                     href="{{ $documentation_url }}"
-                    class="flex items-center mt-2 mx-2 px-4 py-2 rounded-lg border text-gray-700 hover:bg-gray-200 hover:border-gray-500"
+                    class="flex items-center px-4 py-2 mx-2 mt-2 text-gray-700 border rounded-lg hover:bg-gray-200 hover:border-gray-500"
                 >
                     @include('svg.icons.book-reference', ['class' => 'fill-current mr-2 w-4 h-4'])
                     Documentation
@@ -18,7 +18,7 @@
 
                 <a
                     href="https://github.com/JeffreyWay/laravel-mix"
-                    class="flex items-center mt-2 mx-2 px-4 py-2 rounded-lg border text-gray-700 hover:bg-gray-200 hover:border-gray-500"
+                    class="flex items-center px-4 py-2 mx-2 mt-2 text-gray-700 border rounded-lg hover:bg-gray-200 hover:border-gray-500"
                 >
                     @include('svg.icons.github', ['class' => 'fill-current mr-2 w-4 h-4'])
                     Source on Github
@@ -26,7 +26,7 @@
 
                 <a
                     href="https://laracasts.com/series/learn-laravel-mix"
-                    class="flex items-center mt-2 mx-2 px-4 py-2 rounded-lg border text-gray-700 hover:bg-gray-200 hover:border-gray-500"
+                    class="flex items-center px-4 py-2 mx-2 mt-2 text-gray-700 border rounded-lg hover:bg-gray-200 hover:border-gray-500"
                     title="Watch screencasts at Laracasts: Learn Laravel Mix"
                 >
                     @include('svg.icons.video-solid', ['class' => 'fill-current mr-2 w-4 h-4'])
@@ -35,7 +35,7 @@
 
                 <a
                     href="{{ route('extensions.index') }}"
-                    class="flex items-center mt-2 mx-2 px-4 py-2 rounded-lg border text-gray-700 hover:bg-gray-200 hover:border-gray-500"
+                    class="flex items-center px-4 py-2 mx-2 mt-2 text-gray-700 border rounded-lg hover:bg-gray-200 hover:border-gray-500"
                 >
                     @include('svg.icons.puzzle-piece-solid', ['class' => 'fill-current mr-2 w-4 h-4'])
                     Extensions
