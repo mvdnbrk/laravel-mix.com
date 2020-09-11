bfb319294a9f82679543d3110d735ed85062c559
Mark van den Broek <mvdnbrk@gmail.com>
2020-09-11 14:41:21 +0200

    Update card layout

--- resources/views/extensions/index.blade.php
+++ resources/views/extensions/index.blade.php
@@ -14,12 +14,14 @@
         @foreach($extensions as $extension)
         <div class="w-full px-2 mb-4 lg:w-1/2 xl:w-1/3">
             <div class="flex flex-col h-48 bg-white border-t-4 border-blue-400 shadow">
-                <div class="flex flex-col flex-1 p-4">
-                    <a href="{{ route('extensions.show', $extension) }}">
-                        <h2 class="mt-0 mb-2 text-xl text-gray-800 hover:text-blue-500">{{ $extension->title }}</h2>
-                    </a>
-                    <p class="flex-1">{{ \Illuminate\Support\Str::limit($extension->description, 90) }}</p>
-                </div>
+                <a href="{{ route('extensions.show', $extension) }}" class="flex flex-col flex-1 p-4 cursor-pointer">
+                    <h2 class="mt-0 mb-2 text-xl text-gray-900 hover:text-blue-500">
+                        {{ $extension->title }}
+                    </h2>
+                    <p class="flex-1">
+                        {{ \Illuminate\Support\Str::limit($extension->description, 90) }}
+                    </p>
+                </a>
                 <div class="flex justify-between px-4 py-2 text-xs text-blue-700 bg-gray-200 border-t border-gray-400">
                     <div class="font-semibold uppercase">{{ $extension->author_name }}</div>
                     <div class="text-gray-700">{{ $extension->latestVersion }}</div>
