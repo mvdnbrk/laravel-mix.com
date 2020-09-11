4f3fb55c620d1e521bb67a1a57aac44049355d22
Mark van den Broek <mvdnbrk@gmail.com>
2020-09-11 14:08:06 +0200

    Reorder tailwind classes

--- resources/views/extensions/index.blade.php
+++ resources/views/extensions/index.blade.php
@@ -8,12 +8,12 @@
     'hide_menu' => true,
 ])
 
-<div class="max-w-screen-xl mx-auto px-6">
+<div class="max-w-screen-xl px-6 mx-auto">
     <h1 class="py-12 text-center">Discover Laravel Mix Extensions</h1>
     <div class="flex flex-wrap -mx-2 -mb-4">
         @foreach($extensions as $extension)
-        <div class="w-full lg:w-1/2 xl:w-1/3 px-2 mb-4">
-            <div class="flex flex-col h-48 bg-gray-100 shadow border-t-4 border-blue-400">
+        <div class="w-full px-2 mb-4 lg:w-1/2 xl:w-1/3">
+            <div class="flex flex-col h-48 bg-gray-100 border-t-4 border-blue-400 shadow">
                 <div class="flex flex-col flex-1 p-4">
                     <a href="{{ route('extensions.show', $extension) }}">
                         <h2 class="mt-0 mb-2 text-xl text-gray-800 hover:text-blue-500">{{ $extension->title }}</h2>
@@ -23,14 +23,14 @@
                         Learn more
                     </a>
                 </div>
-                <div class="flex justify-between px-4 py-2 bg-gray-200 border-t border-gray-400 text-xs text-blue-700">
+                <div class="flex justify-between px-4 py-2 text-xs text-blue-700 bg-gray-200 border-t border-gray-400">
                     <div class="font-semibold uppercase">{{ $extension->author_name }}</div>
                     <div class="text-gray-700">{{ $extension->latestVersion }}</div>
                 </div>
             </div>
         </div>
         @endforeach
-        <div class="w-full text-center py-6">
+        <div class="w-full py-6 text-center">
             Would you like to be listed on this page? Please open an issue <a href="https://github.com/mvdnbrk/laravel-mix-docs/issues" target="_blank" class="underline hover:no-underline hover:text-gray-900">here</a>
         </div>
     </div>
