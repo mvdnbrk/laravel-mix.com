409d45dc272cf48ac49e1ba69a4947a64388d5d5
Mark van den Broek <mvdnbrk@gmail.com>
2020-09-11 10:07:27 +0200

    Make buttons white

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
             <div class="mt-16 md:flex">
                 <a
                     href="{{ $documentation_url }}"
-                    class="flex items-center px-4 py-2 mx-2 mt-2 text-gray-700 border rounded-lg hover:bg-gray-200 hover:border-gray-500"
+                    class="flex items-center px-4 py-2 mx-2 mt-2 text-gray-700 bg-white border rounded-lg hover:bg-gray-200 hover:border-gray-500"
                 >
                     @include('svg.icons.book-reference', ['class' => 'fill-current mr-2 w-4 h-4'])
                     Documentation
@@ -18,7 +18,7 @@
 
                 <a
                     href="https://github.com/JeffreyWay/laravel-mix"
-                    class="flex items-center px-4 py-2 mx-2 mt-2 text-gray-700 border rounded-lg hover:bg-gray-200 hover:border-gray-500"
+                    class="flex items-center px-4 py-2 mx-2 mt-2 text-gray-700 bg-white border rounded-lg hover:bg-gray-200 hover:border-gray-500"
                 >
                     @include('svg.icons.github', ['class' => 'fill-current mr-2 w-4 h-4'])
                     Source on Github
@@ -26,7 +26,7 @@
 
                 <a
                     href="https://laracasts.com/series/learn-laravel-mix"
-                    class="flex items-center px-4 py-2 mx-2 mt-2 text-gray-700 border rounded-lg hover:bg-gray-200 hover:border-gray-500"
+                    class="flex items-center px-4 py-2 mx-2 mt-2 text-gray-700 bg-white border rounded-lg hover:bg-gray-200 hover:border-gray-500"
                     title="Watch screencasts at Laracasts: Learn Laravel Mix"
                 >
                     @include('svg.icons.video-solid', ['class' => 'fill-current mr-2 w-4 h-4'])
@@ -35,7 +35,7 @@
 
                 <a
                     href="{{ route('extensions.index') }}"
-                    class="flex items-center px-4 py-2 mx-2 mt-2 text-gray-700 border rounded-lg hover:bg-gray-200 hover:border-gray-500"
+                    class="flex items-center px-4 py-2 mx-2 mt-2 text-gray-700 bg-white border rounded-lg hover:bg-gray-200 hover:border-gray-500"
                 >
                     @include('svg.icons.puzzle-piece-solid', ['class' => 'fill-current mr-2 w-4 h-4'])
                     Extensions
