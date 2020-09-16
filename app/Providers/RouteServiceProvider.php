0036accd54b773ca0ac2f685406420b5495b847c
Mark van den Broek <mvdnbrk@gmail.com>
2020-09-16 11:34:14 +0200

    Formatting

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -10,8 +10,7 @@ class RouteServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->routes(function () {
-            Route::middleware('web')
-                ->group(base_path('routes/web.php'));
+            Route::middleware('web')->group(base_path('routes/web.php'));
         });
     }
 }
