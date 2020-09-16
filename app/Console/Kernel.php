fbb976c64e847a6cf5d01b7b4efa3967e5085127
Mark van den Broek <mvdnbrk@gmail.com>
2020-09-16 11:30:32 +0200

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,22 +7,7 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * The Artisan commands provided by your application.
-     *
-     * @var array
-     */
-    protected $commands = [
-        //
-    ];
-
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
-    protected function schedule(Schedule $schedule)
+    protected function schedule(Schedule $schedule): void
     {
         $schedule->command('extension:stats')->daily();
         $schedule->command('extension:refresh')->daily();
@@ -32,12 +17,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('page-cache:clear')->dailyAt('0:30');
     }
 
-    /**
-     * Register the commands for the application.
-     *
-     * @return void
-     */
-    protected function commands()
+    protected function commands(): void
     {
         $this->load(__DIR__.'/Commands');
     }
