37244900b3d77440b15d9773b9437dfb7fc15ec7
Mark van den Broek <mvdnbrk@gmail.com>
2020-09-16 11:27:45 +0200

    Update ExtensionFactory.php

--- database/factories/ExtensionFactory.php
+++ database/factories/ExtensionFactory.php
@@ -6,19 +6,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 
 class ExtensionFactory extends Factory
 {
-    /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    protected $model = \App\Models\Extension::class;
-
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
-    public function definition()
+    public function definition(): array
     {
         return [
             'name' => $this->faker->word,
