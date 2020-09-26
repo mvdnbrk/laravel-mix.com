0cd68e9db33994c369b21a7172fa71a763415cb9
Mark van den Broek <mvdnbrk@gmail.com>
2020-09-26 19:02:09 +0200

    Update schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -9,11 +9,10 @@ class Kernel extends ConsoleKernel
 {
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('extension:stats')->daily();
-        $schedule->command('extension:refresh')->daily();
-        $schedule->command('extension:db-update')->daily();
-        $schedule->command('extension:readme')->daily();
-
+        $schedule->command('extension:stats')->dailyAt('0:10');
+        $schedule->command('extension:refresh')->daily('0:15');
+        $schedule->command('extension:db-update')->daily('0:20');
+        $schedule->command('extension:readme')->daily('0:25');
         $schedule->command('page-cache:clear')->dailyAt('0:30');
     }
 
