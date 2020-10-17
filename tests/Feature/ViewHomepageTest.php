998469453ef7a3deb64a5f849d97e518ee1ac845
Mark van den Broek <mvdnbrk@gmail.com>
2020-10-17 11:36:38 +0200

    Simplify test

--- tests/Feature/ViewHomepageTest.php
+++ tests/Feature/ViewHomepageTest.php
@@ -9,9 +9,6 @@ class ViewHomepageTest extends TestCase
     /** @test */
     public function a_request_to_the_homepage_should_return_a_200_status_code()
     {
-        $response = $this->get('/');
-
-        $response->assertOk();
-        $response->assertViewIs('welcome');
+        $this->get('/')->assertOk()->assertViewIs('welcome');
     }
 }
